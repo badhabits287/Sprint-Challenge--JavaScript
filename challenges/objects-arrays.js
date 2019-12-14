1df3ab187ff3d2b4c64351a591114f4919de7019
badhabits287 <56571933+badhabits287@users.noreply.github.com>
2019-12-13 23:15:15 -0500

    moving on very slowly

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -77,8 +77,13 @@ const graduates = [
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
+
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for(c=0; c<graduates.length; c++){
+  universities.push(graduates[c.university]);
+}
+universities.sort()
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -88,15 +93,14 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
-graduates.forEach(person => 
-contactInfo.push(`${person.first_name}, ${person.email}`));
+for(c=0; c<graduates.length; c++){
+  contactInfo.push((graduates[c].first_name) +" " + (graduates[c].email) )
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-unisWithUni = graduates.filter(function(value){
-  return value.university === "Uni";
-})
+
 console.log(unisWithUni);
 
 
@@ -122,8 +126,13 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const displayNames = [];
+zooAnimals.forEach(function(_animal){
+  displayNames.push(`${_animal.animal_name}`+ " " +`${_animal.scientific_name}`);
+});
 console.log(displayNames);
+
 
 /* Request 2: .map()
 
